397b1cafc223f8e2a3d85885f165f15744b457a8
gabycrem <gabymacre@gmail.com>
2025-01-23 11:48:09 -0300

    Agregar nombre a la lista mostrar Lista en pantalla limpar campo de ingreso de datos

--- app.js
+++ app.js
@@ -14,12 +14,29 @@ function agregarAmigo(){
         alert('Debe ingresar un nombre');
         return;
     }
+    //Agregar nombre a la lista
+    miListaDeAmigos.push(nombreAmigo);
+    //Mostrar Lista de nombres en pantallla
+    nuevoItemLista(nombreAmigo);
+    //Limpiar Campo
+    limpiarCampo('amigo');
 }
 
+//Crea un nuevo elemento 'li' dentro del elemento 'ul' con ID 'listaAmigos'
+function nuevoItemLista(nombre){
+    let nuevoItem = document.createElement('li');
+    nuevoItem.textContent = nombre;
+    listaAmigos.appendChild(nuevoItem);
+    return;
+}
 
-//Validar que no ingrese un campo vacio
-//Agregar nombre a la lista
-//Limpiar Campo
+//Funcion Limpiar Campo
+function limpiarCampo(idElemento){
+    let elementoHTML = document.getElementById(idElemento);
+    elementoHTML.value = ''; //Elimino el valor ingresado
+    elementoHTML.setAttribute('placeHolder', 'Escribe un nombre'); //Reestablezco el mensaje original para el ingreso de datos
+    return;
+}
 //Limpiar Lista
 //Asignar texto
 //Evento Click - 'Sortear Amigo'
